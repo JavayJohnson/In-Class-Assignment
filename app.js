a71b5dd3b125079b5691c8a8c68ba84d366d2005
javay johnson <javayjohnson@gmail.com>
2024-07-10 10:08:53 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Lesson8701</title>
+    <meta name="description" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+
+
+<!-- Nav  -->
+
+<div class="companyContainer">
+
+    <div class="company" id="com_a">
+        <p class="title"> Murray's Pomade </p>
+        <img src="https://shop.murraysworldwide.com/image/cache/catalog/MurraysOriginal-550x550w.jpg
+    </div>
+    <!-- Company A  -->
+
+    <div class="company" id="com_b">
+        <p class="title"> Ebony Magazine </p>
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgiphy.com%2Fexplore%2Fjet-magazine&psig=AOvVaw3lRYlokeEKzncGd3knRdrU&ust=1720694073877000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCJivq4qjnIcDFQAAAAAdAAAAABAE
+    </div>
+    <!-- Company B -->
+
+    <div class="company" id="com_c">
+        <p class="title"> BlackPlanet </p>
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.brandsoftheworld.com%2Flogo%2Fblackplanetcom&psig=AOvVaw3o1SHFZr-rhsZZbIag-tfb&ust=1720693578303000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIChiKyhnIcDFQAAAAAdAAAAABAE alt="Pintrest" class="bizPic">
+    </div>
+    <!-- Company C  -->
+
+    <div class="company" id="com_d">
+        <p class="title">BET</p>
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fmedium.com%2Fthe-brothers%2Fto-us-bet-was-more-than-a-business-it-was-ours-61b772c7d072&psig=AOvVaw1F7XWwaS47bbWlPSHAyPmg&ust=1720693709506000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNCsi-OhnIcDFQAAAAAdAAAAABAQ
+    </div>
+    <!-- Company D  -->
+
+</div>
+        <!-- img -->
+        <!-- p ->displayName -->
+        <!-- button Go to Site -->
+
+    <!-- Company A  -->
+    <!-- Company A  -->
+    <!-- Company A  -->
+
+
+
+
+
+    
+    <script src="app.js" async defer></script>
+  </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
